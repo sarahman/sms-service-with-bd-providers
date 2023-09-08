4fa022fc71dc188cba93e116fd82d38e8bf7b957
Syed Abidur Rahman <abidur.rahman@shohoz.com>
2023-09-08 06:53:23 +0600

    Add BaseProvider for various SMS providers.

---
+++ src/Providers/BaseProvider.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Sarahman\SmsService\Providers;
+
+use Illuminate\Support\Facades\Config;
+use Sarahman\SmsService\Interfaces\ProviderInterface;
+
+abstract class BaseProvider implements ProviderInterface
+{
+    protected $configName;
+    protected $config;
+    protected $url;
+
+    public function __construct(array $config = [], $url = null)
+    {
+        $this->loadConfigFromFile();
+
+        if (!empty($config)) {
+            foreach ($config as $key => $value) {
+                $this->config[$key] = $value;
+            }
+        }
+
+        $this->extractUrlFromConfigAndSet($url);
+    }
+
+    public function setConfigName($configName)
+    {
+        $this->configName = $configName;
+
+        return $this->loadConfigFromFile();
+    }
+
+    public function setConfig(array $config)
+    {
+        $this->config = $config;
+
+        return $this;
+    }
+
+    public function getConfig()
+    {
+        return $this->config;
+    }
+
+    public function setUrl($url)
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    private function loadConfigFromFile()
+    {
+        $this->configName = !is_null($this->configName) ? $this->configName : get_class($this);
+        $this->config = Config::get('sms-service-with-bd-providers::config.providers.' . $this->configName);
+
+        $this->extractUrlFromConfigAndSet();
+
+        return $this;
+    }
+
+    private function extractUrlFromConfigAndSet($url = null)
+    {
+        if (isset($this->config['url'])) {
+            $url = $this->config['url'];
+            unset($this->config['url']);
+        }
+
+        is_null($url) || $this->setUrl($url);
+    }
+}
