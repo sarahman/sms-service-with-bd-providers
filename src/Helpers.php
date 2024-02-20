7ab40606b12b1355417817d9ac73c7667a9421b3
Syed Abidur Rahman <abidur.rahman@shohoz.com>
2024-02-20 13:42:11 +0600

    Refactor according to the StyleCI suggestions.

--- src/Helpers.php
+++ src/Helpers.php
@@ -10,9 +10,9 @@ class Helpers
     public static function getSMSConfig($configName)
     {
         if (version_compare(Application::VERSION, '5.0', '<')) {
-            return Config::get('sms-service-with-bd-providers::config.providers.' . $configName);
+            return Config::get('sms-service-with-bd-providers::config.providers.'.$configName);
         } else {
-            return Config::get('sms-service.providers.' . $configName);
+            return Config::get('sms-service.providers.'.$configName);
         }
     }
 }
